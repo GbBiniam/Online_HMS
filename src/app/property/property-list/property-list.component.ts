135cdf8d6ff465b28718e6a577bd6fc41f7aed11
gbinicho <gbinicho@yahoo.com>
2023-02-24 13:23:38 +0300

    Add card properties

--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -3,13 +3,48 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
-  styleUrls: ['./property-list.component.css']
+  styleUrls: ['./property-list.component.css'],
 })
 export class PropertyListComponent implements OnInit {
+  Property: any = [
+    {
+      Id: 1,
+      Type: 'House',
+      Price: 4055520,
+      Name: 'Vila House',
+    },
+    {
+      Id: 2,
+      Type: 'Appartama',
+      Price: 12055520,
+      Name: '3 Bed House',
+    },
+    {
+      Id: 3,
+      Type: 'Condominium',
+      Price: 355520,
+      Name: '3 Bed House',
+    },
+    {
+      Id: 4,
+      Type: 'Stair',
+      Price: 12055520,
+      Name: 'Stair House',
+    },
+    {
+      Id: 1,
+      Type: 'Ground',
+      Price: 255520,
+      Name: 'Ground House',
+    },
+    {
+      Id: 1,
+      Type: 'Paint',
+      Price: 1955520,
+      Name: 'Paint House',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
