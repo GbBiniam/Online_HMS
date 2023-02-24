e586dc49af292eb7638a7431708c533ccd9083ab
gbinicho <gbinicho@yahoo.com>
2023-02-24 12:48:38 +0300

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PropertyCardComponent } from './property/property-card/property-card.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { PropertyListComponent } from './property/property-list/property-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PropertyCardComponent,
+    PropertyListComponent,
+    NavBarComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
